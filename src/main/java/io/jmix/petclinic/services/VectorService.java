369faafde9adec3a6d8c0b5b199220395b6bae45
Kevin Phan <phan_duylx@yahoo.com>
2026-01-16 06:23:04 -0600

    Pushed file to Embeding service, but embedding value is null

--- src/main/java/io/jmix/petclinic/services/VectorService.java
+++ src/main/java/io/jmix/petclinic/services/VectorService.java
@@ -1,18 +1,28 @@
 package io.jmix.petclinic.services;
 
 
+import io.jmix.core.FileRef;
+import io.jmix.core.FileStorage;
 import io.jmix.petclinic.response.ImageVectorEmbeddingResponse;
+import io.jmix.petclinic.utils.SimpleMultipartFileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Service
@@ -21,18 +31,24 @@ public class VectorService {
     @Autowired
     private WebClient webClientVector;
 
-    public List<Float> getEmbeddingVector(File image) {
-        MultipartBodyBuilder builder = new MultipartBodyBuilder();
-        builder.part("file", image)
-                .header(HttpHeaders.CONTENT_DISPOSITION, "form-data; name=\"file\"; filename=\"" + image.getName() + "\"");
+    @Autowired
+    private FileStorage fileStorage;
 
-        MultiValueMap<String, HttpEntity<?>> multipartBody = builder.build();
+    public List<Float> getEmbeddingVector(FileRef image) throws Exception {
+        final String formFieldName = "file";
+        InputStreamResource resource = new InputStreamResource(fileStorage.openStream(image));
+        MultiValueMap<String, Object> form = new LinkedMultiValueMap<>();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        headers.setContentDisposition(ContentDisposition.formData().filename(image.getFileName()).name(formFieldName).build());
+        HttpEntity<InputStreamResource> entity = new HttpEntity<>(resource, headers);
+        form.add(formFieldName, entity);
 
 
 
         ImageVectorEmbeddingResponse imageVectorEmbeddingResponse = webClientVector.post().uri("/extract-vector")
                 .contentType(MediaType.MULTIPART_FORM_DATA)
-                .body(BodyInserters.fromMultipartData(multipartBody))
+                .body(BodyInserters.fromMultipartData(form))
                 .retrieve().bodyToMono(ImageVectorEmbeddingResponse.class).block();
 
         return imageVectorEmbeddingResponse.getEmbedding();
--- src/main/java/io/jmix/petclinic/view/pet/pet/PetDetailView.java
+++ src/main/java/io/jmix/petclinic/view/pet/pet/PetDetailView.java
@@ -1,11 +1,33 @@
 package io.jmix.petclinic.view.pet.pet;
 
+import io.jmix.core.FileRef;
+import io.jmix.core.FileStorage;
+import io.jmix.flowui.component.upload.FileStorageUploadField;
+import io.jmix.flowui.component.upload.receiver.FileTemporaryStorageBuffer;
+import io.jmix.flowui.kit.component.upload.event.FileUploadSucceededEvent;
+import io.jmix.flowui.upload.TemporaryStorage;
 import io.jmix.petclinic.entity.pet.Pet;
 
+import io.jmix.petclinic.services.VectorService;
 import io.jmix.petclinic.view.main.MainView;
 
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.view.*;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.UUID;
+
 
 @Route(value = "pets/:id", layout = MainView.class)
 @ViewController("petclinic_Pet.detail")
@@ -13,4 +35,41 @@ import io.jmix.flowui.view.*;
 @EditedEntityContainer("petDc")
 @DialogMode
 public class PetDetailView extends StandardDetailView<Pet> {
+
+    private static final Logger log = LoggerFactory.getLogger(PetDetailView.class);
+    @ViewComponent
+    private FileStorageUploadField avatarField;
+
+    @Autowired
+    private TemporaryStorage temporaryStorage;
+    @Autowired
+    private VectorService vectorService;
+    @Autowired
+    private FileStorage fileStorage;
+
+    @Subscribe("avatarField")
+    public void onAvatarFieldFileUploadSucceeded(final FileUploadSucceededEvent<FileStorageUploadField> event)  {
+        if (event.getReceiver() instanceof FileTemporaryStorageBuffer buffer) {
+            UUID fileId = buffer.getFileData().getFileInfo().getId();
+            File file = temporaryStorage.getFile(fileId);
+
+            if (file != null) {
+                // get vector
+                //File fileToGetVector = new File(file.getPath());
+                FileRef fileRef = temporaryStorage.putFileIntoStorage(fileId, event.getFileName());
+                avatarField.setValue(fileRef);
+
+
+                //File fileToSend =  File.createTempFile("original-image", "."+FilenameUtils.getExtension(event.getFileName()));
+                //fileToSend.deleteOnExit();
+                //FileUtils.copyInputStreamToFile(fileStorage.openStream(fileRef), fileToSend);
+                try {
+                    List<Float> result = vectorService.getEmbeddingVector(fileRef);
+                } catch (Exception ex) {
+                    log.error(ex.toString());
+                }
+
+            }
+        }
+    }
 }
